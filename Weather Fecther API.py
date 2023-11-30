9a6b77b0d533ea6fb4c6bb5c5633056a6e10529b
Hassan <133172426+hsyed24@users.noreply.github.com>
2023-11-30 08:57:00 +0000

    Python projects
    Some projects that I'm working on

---
+++ Weather Fecther API.py
@@ -0,0 +1,20 @@
+import requests
+
+#The API key was removed because of security purposes
+
+API_KEY = "ENTER API KEY"
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+
+city = input("Please enter a city name: ")
+request_url = f"{BASE_URL}?appid={API_KEY}&q={city}&units=metric"
+response = requests.get(request_url)
+
+if response.status_code == 200:
+    data = response.json()
+    weather = data['weather'][0]['description']
+    temperature = round(data["main"]["temp"], 2)
+
+    print("Weather:", weather)
+    print("Temperature:", temperature, "celsius")
+else:
+    print("Oops..., something went wrong.")
